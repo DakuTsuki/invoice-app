6dd07a7fc2118dfe312aab549bdce98cce644828
Dakutsuki <demomega9@gmail.com>
2024-12-05 12:31:36 +0100

    push

---
+++ src/app/invoices/new/page.tsx
@@ -0,0 +1,53 @@
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-baseline justify-center  gap-6 max-w-5xl mx-auto my-12">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold text-left">Create Invoice</h1>
+      </div>
+
+      <form className="grid gap-4 max-w-xs">
+        <div>
+          <Label className="block mb-2 font-semibold text-small" htmlFor="name">
+            Billing Name
+          </Label>
+          <Input id="name" name="name" type="text" />
+        </div>
+        <div>
+          <Label
+            className="block mb-2 font-semibold text-small"
+            htmlFor="email"
+          >
+            Billing Email
+          </Label>
+          <Input id="name" name=" email" type="email" />
+        </div>
+        <div>
+          <Label
+            className="block mb-2 font-semibold text-small"
+            htmlFor="value"
+          >
+            Value
+          </Label>
+          <Input id="name" name=" value" type="text" />
+        </div>
+        <div>
+          <Label
+            className="block mb-2 font-semibold text-small"
+            htmlFor="description"
+          >
+            Description
+          </Label>
+          <Textarea name="description" id="description"></Textarea>
+        </div>
+        <div>
+          <Button className="w-full font-semibold">Submit</Button>
+        </div>
+      </form>
+    </main>
+  );
+}
