dca25e675d5a04b330b169688e4bb3dcf4eb6b85
Dakutsuki <demomega9@gmail.com>
2024-12-05 14:03:05 +0100

    push

---
+++ src/app/action.ts
@@ -0,0 +1,5 @@
+'use Server'
+
+async function createAction(formData: FormData){
+  console.log('formData', formData)
+}
--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,16 +1,21 @@
+import { sql } from "drizzle-orm";
+
+import { db } from "@/db";
+
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { createAction } from "@/app/action"
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className="flex flex-col items-baseline justify-center  gap-6 max-w-5xl mx-auto my-12">
       <div className="flex justify-between">
         <h1 className="text-3xl font-bold text-left">Create Invoice</h1>
       </div>
 
-      <form className="grid gap-4 max-w-xs">
+      <form action={createAction} className="grid gap-4 max-w-xs">
         <div>
           <Label className="block mb-2 font-semibold text-small" htmlFor="name">
             Billing Name
